b296db17cb11a11025057944b5202155f4729a2a
Veronika Kiss <kissv@flip3.engr.oregonstate.edu>
2024-02-14 11:42:31 -0800

    add CRUD operations for Authors table

--- DML.sql
+++ DML.sql
@@ -20,7 +20,13 @@ SET Books.title = :titleInput, Books.authorID = (SELECT authorID FROM Authors WH
     
 DELETE FROM Books WHERE Books.title = :titleInput;
 
+--Authors table CRUD operations
+SELECT authors.authorID, authors.authorName, Publishers.publisherName as Publisher
+    FROM Authors
+    INNER JOIN Publishers ON Publishers.publisherID = Authors.publisherID;
 
+INSERT INTO Authors (authorName, publisherID)
+VALUES (:authorInput, (SELECT publisherID FROM Publishers WHERE :publisherInput = Publishers.publisherName));
 
 -- Purchases table CRUD operations
 SELECT Purchases.purchaseID, Customers.customerName as Customer, Purchases.datePlaced, Purchases.totalPrice, 
